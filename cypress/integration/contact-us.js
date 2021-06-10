639c4b6a3c09a84d420bb07adef048bd04ed99c5
MarcinKufel <marcinkufel84@gmail.com>
2021-06-10 06:28:26 +0200

    added click action to test

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -3,6 +3,7 @@
 describe("Test Contact Us via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#contact-us > .thumbnail').click()
     });
     
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
