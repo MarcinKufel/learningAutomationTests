0ba8aad7e1ecf82ecf94746fa4d703879bd24b47
MarcinKufel <marcinkufel84@gmail.com>
2021-06-10 07:16:12 +0200

    added first type words in a form

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -2,8 +2,13 @@
 
 describe("Test Contact Us via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit('http://www.webdriveruniversity.com/')
-        cy.get('#contact-us').click({force: true})
+//        cy.visit('http://www.webdriveruniversity.com/')
+//        cy.get('#contact-us').click({force: true})
+          cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+          cy.get('[name="first_name"]').type('Joe')
+          cy.get('[name="last_name"]').type('Smith')
+          cy.get('[name="email"]').type('JohnSmith@o2.pl')
+          cy.get('textarea.feedback-input').type('We need more practice with cypress')
     });
     
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
