8eccdb312ed05b16822c92c9c3aed30795f4bc76
MarcinKufel <marcinkufel84@gmail.com>
2021-06-10 07:41:05 +0200

    second test case added with negative scenario

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -4,15 +4,20 @@ describe("Test Contact Us via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
 //        cy.visit('http://www.webdriveruniversity.com/')
 //        cy.get('#contact-us').click({force: true})
-          cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
-          cy.get('[name="first_name"]').type('Joe')
-          cy.get('[name="last_name"]').type('Smith')
-          cy.get('[name="email"]').type('JohnSmith@o2.pl')
-          cy.get('textarea.feedback-input').type('We need more practice with cypress')
-          cy.get('[type="submit"]').click()
+          cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+          cy.get('[name="first_name"]').type('Joe');
+          cy.get('[name="last_name"]').type('Smith');
+          cy.get('[name="email"]').type('JohnSmith@o2.pl');
+          cy.get('textarea.feedback-input').type('We need more practice with cypress');
+          cy.get('[type="submit"]').click();
     });
     
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        //cypress code
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.get('[name="first_name"]').type('Tom');
+        cy.get('[name="last_name"]').type('Smith');
+   //     cy.get('[name="email"]').type('JohnSmith@o2.pl');
+        cy.get('textarea.feedback-input').type('We need more practice with cypress');
+        cy.get('[type="submit"]').click();
     });
 })
