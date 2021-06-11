a03e3a895b7332c55a0d92e51b17251feb158014
MarcinKufel <marcinkufel84@gmail.com>
2021-06-11 12:32:56 +0200

    add type and submit click to form

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -6,6 +6,8 @@ describe("Test Contact Us via Automation test store", () => {
         cy.get('.info_links_footer > :nth-child(5) > a').click();
         cy.get('#ContactUsFrm_first_name').type("Jon");
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?");
+        cy.get('.col-md-6 > .btn').click();
     });
     
 
