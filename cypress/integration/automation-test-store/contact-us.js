7ad392803aa5f8620d98a06e583635ac6d332ef6
MarcinKufel <marcinkufel84@gmail.com>
2021-06-11 08:13:07 +0200

    added few tests to contact form

---
+++ cypress/integration/automation-test-store/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us via Automation test store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get('#ContactUsFrm_first_name').type("Jon");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+    });
+    
+
+})
