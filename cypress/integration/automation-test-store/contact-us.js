b3b88ea84fdfefa44ad1ea9f88fbb71013f7e7ef
MarcinKufel <marcinkufel84@gmail.com>
2021-06-17 08:20:07 +0200

    added promise and cy.log to our test

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -4,7 +4,9 @@ describe("Test Contact Us via Automation test store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
         //cy.get('.info_links_footer > :nth-child(5) > a').click();
-        cy.get("a[href$='contact']").click();
+        cy.get("a[href$='contact']").click().then((contactUsText) => {
+            cy.log("Clicked on link using "+ contactUsText.text())
+        });
         //cy.xpath("//a[contains(@href, 'contact')]").click();
         cy.get('#ContactUsFrm_first_name').type("Jon");
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
