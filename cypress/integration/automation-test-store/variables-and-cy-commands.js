e4d44ae2f3a7f26b4ad9b5fe06a191023bd328a9
MarcinKufel <marcinkufel84@gmail.com>
2021-06-18 07:39:35 +0200

    created two variables with selectors

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
 
-describe("Inspect Automation Test Store items using chain of commands", () => {
-    it("Click on the first item using item header", () => {
-        cy.visit("https://www.automationteststore.com/");
-        cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
+describe("Verifying variables, cypress commands and jquery commands", () => {
+    it("Navigating to specific product pages", () => {
+        const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup");
+        const skinCare = cy.get("a[href*='product/category&path=']").contains("Skincare");
+
     }); 
 
 })
