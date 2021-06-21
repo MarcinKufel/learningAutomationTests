476260e52271568d9eea072372a3e70eb7efca57
MarcinKufel <marcinkufel84@gmail.com>
2021-06-21 07:12:27 +0200

    added example from cypress.io documentation

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -4,15 +4,24 @@ describe("Iterate over elements", () => {
     it("Log information of all hair care products ", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-
-    }); 
+        cy.get('ul>li').each(($el, index, $list) => {
+            // $el is a wrapped jQuery element
+            if ($el.someMethod() === 'something') {
+                // wrap this element so we can
+                // use cypress commands on it
+                cy.wrap($el).click()
+            } else {
+                // do something else
+            }
+        })
+    });
     it("Add specific product to basket", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
-    }); 
+    });
 
- 
+
 
 
 })
