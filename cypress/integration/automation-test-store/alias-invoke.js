3733d80e28db8ddce52859e29d7601bc19d9ca66
MarcinKufel <marcinkufel84@gmail.com>
2021-06-21 08:23:01 +0200

    added new test with alias

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -9,4 +9,13 @@ describe("Alias and invoke", () => {
         cy.get('@productThumbnail').its('length').should('be.gt', 5);
         cy.get('@productThumbnail').should('include', 'Seaweed Conditioner');
         });
-    })
+
+
+        it.only("Validate product thumbnail", () => {
+            cy.visit("https://www.automationteststore.com/");
+            cy.get('.thumbnail').as('productThumbnail');
+            cy.get('@productThumbnail').should('have.length', 16);
+            cy.get('@productThumbnail').find('.productcart').invoke('attr', 'title').should('include', 'Add to Cart');
+
+    });
+})
