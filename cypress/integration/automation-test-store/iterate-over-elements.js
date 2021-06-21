4ea9edebce742e5481144d2b12337c610d7b1938
MarcinKufel <marcinkufel84@gmail.com>
2021-06-21 07:39:31 +0200

    added iterated code to cypress

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -4,21 +4,21 @@ describe("Iterate over elements", () => {
     it("Log information of all hair care products ", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-        cy.get('ul>li').each(($el, index, $list) => {
-            // $el is a wrapped jQuery element
-            if ($el.someMethod() === 'something') {
-                // wrap this element so we can
-                // use cypress commands on it
-                cy.wrap($el).click()
-            } else {
-                // do something else
-            }
-        })
+        
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            cy.log("Index: " + index + " : " + $el.text())
+    
+        });
     });
     it("Add specific product to basket", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-
+        
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo')) {
+                cy.wrap($el).click()
+            }
+        });
     });
 
 
