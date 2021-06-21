ce78693bc39d43e459beb2840194ec4ba1cd237d
MarcinKufel <marcinkufel84@gmail.com>
2021-06-21 08:00:58 +0200

    added invoke and alias to tests

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -6,6 +6,7 @@ describe("Alias and invoke", () => {
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
         
         cy.get('.fixed_wrapper .prdocutname').eq(0).invoke('text').as('productThumbnail');
-    
+        cy.get('@productThumbnail').its('length').should('be.gt', 5);
+        cy.get('@productThumbnail').should('include', 'Seaweed Conditioner');
         });
     })
