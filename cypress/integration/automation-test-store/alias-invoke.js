e3554dd9675041d75b362de3ddf0d4e4c79e5a63
MarcinKufel <marcinkufel84@gmail.com>
2021-06-24 08:30:31 +0200

    counted price of products

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -25,14 +25,38 @@ describe("Alias and invoke", () => {
         // cy.get('@productThumbnail').find('.oneprice').each(($el, index, $list) => {
         //     cy.log($el.text());
         //   });
-            cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice')
+            cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice');
+            cy.get('.thumbnail').find('.pricenew').invoke('text').as('saleItemPrice')
+
+
+            let itemsTotal = 0;
             cy.get('@itemPrice').then($linkText => {
+                let itemsPriceTotal = 0;
                 let itemPrice = $linkText.split('$');
                 let i;
                 for(i = 0; i < itemPrice.length; i++){
-                  cy.log(itemPrice[i])
+                  cy.log(itemPrice[i]) 
+                  itemsPriceTotal += Number(itemPrice[i])
                 }
+
+                itemsTotal += itemsPriceTotal;
+                cy.log("Non sale price items total: " + itemsPriceTotal)
             })
         
+            cy.get('@saleItemPrice').then($linkText => {
+                let saleItemsPrice = 0;
+                let saleItemPrice = $linkText.split('$');
+                let i;
+                for(i = 0; i < saleItemPrice.length; i++){
+                    cy.log(saleItemPrice[i]) 
+                    saleItemsPrice += Number(saleItemPrice[i])
+                  }
+                  itemsTotal += saleItemsPrice;
+                  cy.log("Sale price items total: " + saleItemsPrice)
+            })
+            .then(() => {
+                cy.log("The total price of all products: " + itemsTotal)
+                expect(itemsTotal).to.equal(640.7)
+                    })
     });
 })
