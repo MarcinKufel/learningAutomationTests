eb69abab0390d5b71fb8e00321e24b052027e37b
MarcinKufel <marcinkufel84@gmail.com>
2021-06-28 06:36:02 +0200

    started write tests to open links in the same tab

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -2,9 +2,9 @@
 
 describe("Test Contact Us via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-//        cy.visit('http://www.webdriveruniversity.com/')
+        cy.visit('http://www.webdriveruniversity.com/')
 //        cy.get('#contact-us').click({force: true})
-          cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+       //   cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
           cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
           cy.title().should('include', 'WebDriver | Contact Us');
           cy.url().should('include', 'contactus');
@@ -17,7 +17,8 @@ describe("Test Contact Us via WebdriverUni", () => {
     });
     
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+      //  cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.visit('http://www.webdriveruniversity.com/')
         cy.get('[name="first_name"]').type('Tom');
         cy.get('[name="last_name"]').type('Smith');
    //   cy.get('[name="email"]').type('JohnSmith@o2.pl');
