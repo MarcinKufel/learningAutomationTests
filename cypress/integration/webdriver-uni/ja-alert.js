43c9ba5ad7abff86757dfd0853d59b9a925aadd4
MarcinKufel <marcinkufel84@gmail.com>
2021-06-30 07:27:45 +0200

    added test to validate js confirm button

--- cypress/integration/webdriver-uni/ja-alert.js
+++ cypress/integration/webdriver-uni/ja-alert.js
@@ -35,4 +35,22 @@ describe("Handle js alerts", () => {
         })
         cy.get('#confirm-alert-text').contains('You pressed Cancel!')
     });
+
+    it("Validate js confirm alert box using a stub", () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true});
+
+        const stub = cy.stub();
+        cy.on('window:confirm', stub); 
+        cy.get('#button4').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+        }).then(()=> {
+            return true;
+        }).then(() => {
+            cy.get('#confirm-alert-text').contains('You pressed OK!')
+
+        })
+ 
+   
+    });
 })
