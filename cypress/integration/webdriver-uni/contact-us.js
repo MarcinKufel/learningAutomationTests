9492a4157639a1fbb2c282707bf9a2b531e980f6
MarcinKufel <marcinkufel84@gmail.com>
2021-07-13 07:25:15 +0200

    added page object modeling

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,4 +1,6 @@
 import Homepage_PO from '../../support/pageObjects/webdriver-uni/Homepage_PO'
+import Contact_Us_PO from '../../support/pageObjects/webdriver-uni/Contact_Us_PO'
+
 
 /// <reference types="cypress" />
 
@@ -32,8 +34,9 @@ describe("Test Contact Us via WebdriverUni", () => {
           // cy.get('[type="submit"]').click();
           // cy.get('h1').should('have.text', 'Thank You for your Message!')
          // cy.webdriverUni_ContactForm_Submission(data.first_name, data.last_name, data.email, 'We need more practice with cypress', 'h1', 'Thank You for your Message!')
-            cy.webdriverUni_ContactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, 'We need more practice with cypress', 'h1', 'Thank You for your Message!')
-
+         //   cy.webdriverUni_ContactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, 'We need more practice with cypress', 'h1', 'Thank You for your Message!')
+        const contact_Us_PO = new Contact_Us_PO();
+        contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, 'We need more practice with cypress', 'h1', 'Thank You for your Message!');
     });
     
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
