e546bffa3c3e3fb6bf8f44fe90b20e3f70e846cc
MarcinKufel <marcinkufel84@gmail.com>
2021-07-15 06:38:26 +0200

    added next code to page objects

--- cypress/integration/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/integration/automation-test-store/add-multiple-items-to-basket.js
@@ -1,9 +1,13 @@
 import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO'
+import AutoStore_HairCare_PO from '../../support/pageObjects/automation-test-store/AutoStore_HairCare_PO'
+
 
 /// <reference types="cypress" />
 
 describe("Add multiple items to basket", () => {
     const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+    const autoStore_HairCare_PO = new AutoStore_HairCare_PO();
+
 
     before(() => {
         cy.fixture("products").then((data) => {
@@ -15,9 +19,10 @@ describe("Add multiple items to basket", () => {
         autoStore_Homepage_PO.clickOn_HairCare_Link();
     })
     it("Add specific items to basket", () => {
-        globalThis.data.productName.forEach((element) => {
-            cy.addProductToBasket(element)
-        })
-        cy.get('.dropdown-toggle > .fa').click
+        autoStore_HairCare_PO.addHairCareProductsToBasket();
+        // globalThis.data.productName.forEach((element) => {
+        //     cy.addProductToBasket(element)
+        // })
+        // cy.get('.dropdown-toggle > .fa').click
     });
 })
---
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -0,0 +1,10 @@
+class AutoStore_HairCare_Po {
+    addHairCareProductsToBasket() {
+        globalThis.data.productName.forEach((element) => {
+            cy.addProductToBasket(element)
+        })
+        cy.get('.dropdown-toggle > .fa').click
+    }
+}
+
+export default AutoStore_HairCare_Po;
